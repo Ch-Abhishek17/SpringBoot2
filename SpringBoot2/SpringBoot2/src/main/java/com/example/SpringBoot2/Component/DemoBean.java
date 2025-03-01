b228812954471857f7a5b81a2bc4e1714f7fd561
Ch-Abhishek17 <abhimanyu17abd@gmail.com>
2025-03-01 22:50:42 +0530

    Added to UC2

---
+++ SpringBoot2/SpringBoot2/src/main/java/com/example/SpringBoot2/Component/DemoBean.java
@@ -0,0 +1,25 @@
+package com.example.SpringBoot2.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoBean.class);
+
+    public DemoBean() {
+        logger.info("DemoBean initialized!");
+    }
+
+    public String sayHello() {
+        logger.debug("Executing sayHello() method.");
+        return "Hello from DemoBean!";
+    }
+
+    @Override
+    public String toString() {
+        return "DemoBean is initialized!";
+    }
+}
--- SpringBoot2/SpringBoot2/src/main/java/com/example/SpringBoot2/SpringBoot2Application.java
+++ SpringBoot2/SpringBoot2/src/main/java/com/example/SpringBoot2/SpringBoot2Application.java
@@ -1,19 +1,23 @@
 package com.example.SpringBoot2;
-
+import com.example.SpringBoot2.Component.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBoot2Application {
 
-	public static void main(String[] args) {
+	public static final Logger logger = LoggerFactory.getLogger(SpringBoot2Application.class);
 
-		SpringApplication.run(SpringBoot2Application.class, args);
-	}
-		System.out.println("Welcome to Spring Concept Demo");
+	public static void main(String[] args) {
+		logger.debug("Welcome to Spring Concept Demo");
 
 		ApplicationContext context = SpringApplication.run(SpringBoot2Application.class, args);
 
-		System.out.println("Checking Context: " + context.getBean(DemoBean.class));
+		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
 
+		logger.debug("\n*** Example Using @Autowired annotation on property ***");
+	}
 }
